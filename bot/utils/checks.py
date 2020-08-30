5e2bc15c1374186fe5bd2ebd7ca5e57417aa0b36
vcokltfre <vcokltfre@gmail.com>
2020-08-30 07:58:46 +0100

    feat: add permissions system

--- bot/utils/checks.py
+++ bot/utils/checks.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 
+from .permissions import PermissionManager
 from config.config import dev_ids
 
 def is_dev():
@@ -8,8 +9,18 @@ def is_dev():
 
     return commands.check(check)
 
-def permitted(level):
+def permitted(level: int):
     async def check(ctx: commands.Context):
-        return True
+        roles = [role.name for role in ctx.author.roles]
+        uid = str(ctx.author.id)
+
+        valid = PermissionManager().has_perms(roles, uid, level)
+
+        if valid:
+            return True
+
+        if not ctx.message.content.startswith("!help"):
+            await ctx.send(f"You do not have the required permission level ({level}) required to run this command.", delete_after=15)
+        return False
 
     return commands.check(check)
