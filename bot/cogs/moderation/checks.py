fc8626ff169147cb398354246dff2aa3f49a1d1e
vcokltfre <vcokltfre@gmail.com>
2020-08-30 06:47:52 +0100

    feat: add role utils to mod checks

--- bot/cogs/moderation/checks.py
+++ bot/cogs/moderation/checks.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 exempt_roles = ["Administrator", "Moderator", "SalC1 Bot", "SalC1 Bot - LP", "Dyno", "Dyno Premium", "BetterAntispam"]
 
@@ -10,3 +11,14 @@ async def punish_check(ctx: commands.Context, member: discord.Member):
             await ctx.channel.send("You are not permitted to punish this member.")
             raise PermissionError()
     return True
+
+async def apply_role(guild: discord.Guild, member: discord.Member, role: str, reason: str = "No reason given"):
+    role = get(guild.roles, name=role)
+    await member.add_roles(role, reason=reason)
+
+async def remove_role(guild: discord.Guild, member: discord.Member, role: str, reason: str = "No reason given"):
+    role = get(guild.roles, name=role)
+    await member.remove_roles(role, reason=reason)
+
+def has_role(member: discord.Member, role: str):
+    return role in [role.name for role in member.roles]
